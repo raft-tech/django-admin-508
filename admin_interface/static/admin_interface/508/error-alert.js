d004f5f005750f589440bd280ad539f3af6bb7a9
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2021-07-19 16:26:25 -0400

    fix: Add ARIA alert to form submission errors

---
+++ admin_interface/static/admin_interface/508/error-alert.js
@@ -0,0 +1,15 @@
+/** global: django */
+
+if (typeof (django) !== 'undefined' && typeof (django.jQuery) !== 'undefined') {
+  (function ($) {
+    'use strict';
+    $(document).ready(function () {
+      const errors = document.querySelectorAll('.errornote')
+
+      // Add ARIA role to error elements so they are read by screen readers.
+      for (const error of errors) {
+        error.setAttribute('role', 'alert')
+      }
+    });
+  })(django.jQuery);
+}
