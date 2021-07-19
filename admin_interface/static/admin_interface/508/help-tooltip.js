3b530d2d8c479aff664185cec9d84e235c3709b1
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2021-07-19 13:28:26 -0400

    fix: Implement accessible ARIA markup on tooltips

---
+++ admin_interface/static/admin_interface/508/help-tooltip.js
@@ -0,0 +1,16 @@
+/** global: django */
+
+if (typeof(django) !== 'undefined' && typeof(django.jQuery) !== 'undefined') {
+    (function($) {
+        'use strict';
+        $(document).ready(function() {
+          const tooltips = document.querySelectorAll('.help-tooltip')
+
+          // Add role="tooltip" and tabindex="0", allowing tooltips to be read by screen readers.
+          for (const tooltip of tooltips) {
+            tooltip.setAttribute('role', 'tooltip')
+            tooltip.setAttribute('tabindex', '0')
+          }
+        });
+    })(django.jQuery);
+}
