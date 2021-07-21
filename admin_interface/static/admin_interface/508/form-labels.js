341bc3fee0ef717f67c6836ee372796508db1f5c
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2021-07-21 16:36:15 -0400

    fix: Form labels and addition a11y warnings

--- admin_interface/static/admin_interface/508/form-labels.js
+++ admin_interface/static/admin_interface/508/form-labels.js
@@ -11,6 +11,13 @@ if (typeof (django) !== 'undefined' && typeof (django.jQuery) !== 'undefined') {
         const title = select?.['data-field-name'] ?? select?.name
         select.setAttribute('title', title.replace(/_/g, ' '))
       }
+
+      // Add missing label to time inputs in change forms.
+      const inputs = document.querySelectorAll('input.vTimeField')
+
+      for (const input of inputs) {
+        input.title = input.name.replace(/_/g, ' ')
+      }
     });
   })(django.jQuery);
 }
