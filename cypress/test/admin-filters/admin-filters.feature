d531e2293f5ba70372cd363f5c2451f31283c3cf
Eric Lipe <elipe@teamraft.com>
2024-06-13 14:27:09 -0400

    - Added test for multiselect filtering

--- cypress/test/admin-filters/admin-filters.feature
+++ cypress/test/admin-filters/admin-filters.feature
@@ -4,9 +4,13 @@ Feature: Admin Console Filtering
         When The admin navigates to the users page
         And The admin filters for user ...
         Then Only the filtered user is displayed
-    Scenario: An Admin User can Create a New Filter and Use it
+    Scenario: An Admin User can use the MultiSelectFilter
       Given The admin logs in
-        When The admin navigates to the users page
-        And The admin creates a filter
-        Then The admin uses the new filter
-        And Only the filtered data is displayed
+        When The admin navigates to the dummies page
+        And The admin uses the multiselect filter
+        Then Only the filtered data is displayed
+    Scenario: An Admin User can clear Filters
+      Given The admin logs in
+        When The admin navigates to the dummies page
+        And The admin uses the multiselect filter
+        Then The admin clears the filters
--- cypress/test/admin-filters/admin-filters.js
+++ cypress/test/admin-filters/admin-filters.js
@@ -1,18 +1,15 @@
 import { Then, When } from '@badeball/cypress-cucumber-preprocessor'
 
-// Shared
+// Scenario: An Admin User can Filter Users
 When('The admin navigates to the users page', () => {
-    cy.get('a[href="/admin/auth/user/"]').first().click().then(() => {
-        cy.get('table[id="result_list"]').find("tr").should('have.length', 2)
-    })
+    cy.get('a[href="/admin/auth/user/"]').first().click()
+    cy.get('table[id="result_list"]').find("tr").should('have.length', 2)
 })
 
-// Scenario: An Admin User can Filter Users
 When('The admin filters for user ...', () => {
     cy.get('select[title="superuser status"]').select('Yes')
-    cy.get('input[type="submit"]').last().click().then(() => {
-        cy.get('table[id="result_list"]').find("tr").should('have.length', 2)
-    })
+    cy.get('input[type="submit"]').last().click()
+    cy.get('table[id="result_list"]').find("tr").should('have.length', 2)
 })
 
 Then('Only the filtered user is displayed', () => {
@@ -20,15 +17,26 @@ Then('Only the filtered user is displayed', () => {
     cy.get('table[id="result_list"]').find("tr").should('have.length', 2)
 })
 
-// Scenario: An Admin User can Create a New Filter and Use it
-When('The admin creates a filter', () => {
-
+// Scenario: An Admin User can use the MultiSelectFilter
+When('The admin navigates to the dummies page', () => {
+    cy.get('a[href="/admin/tests/dummymodel/"]').first().click()
+    cy.get('table[id="result_list"]').find("tr").should('have.length', 5)
 })
 
-Then('The admin uses the new filter', () => {
-
+Then('The admin uses the multiselect filter', () => {
+    cy.get('a[href="?name=Bill"]').first().click()
+    cy.get('table[id="result_list"]').find("tr").should('have.length', 2)
+    cy.get('a[href="?name=Bill%2CDiane"]').first().click()
 })
 
 When('Only the filtered data is displayed', () => {
+    cy.get('table[id="result_list"]').find("tr").should('have.length', 3)
+    cy.url().should('eq', `${Cypress.env('adminUrl')}/tests/dummymodel/?name=Bill%2CDiane`)
+})
 
+// Scenario: An Admin User can clear Filters
+Then('The admin clears the filters', () => {
+    cy.get('a[href="?"]').first().click()
+    cy.get('table[id="result_list"]').find("tr").should('have.length', 5)
+    cy.url().should('eq', `${Cypress.env('adminUrl')}/tests/dummymodel/?`)
 })
